c9fd85e03d734bd5b1ab296fc9c76c47c53877d4
hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>
2022-07-04 20:06:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'hộp quà bí mật tặng cho';
+const titleCard = 'Hello';
+const contentCard = 'mình còn mỗi cái nịt cho bạn';
+const giftImage = 'lol.png';
+//const base64 = '';
+//const giftImageBase64 = "data:image/png;base64, " + base64;
